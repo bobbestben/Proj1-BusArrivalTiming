91e5e7fc0cf1bf6c4393f0670069973c593e8b25
bobbest <banditben.1994@gmail.com>
2022-06-30 22:33:41 +0800

    hosting on github page

---
+++ test.js
@@ -0,0 +1,26 @@
+// Get temporary access to cors here first:
+// https://cors-anywhere.herokuapp.com/corsdemo
+
+
+async function fetchDataAsync(url) {
+    try {
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'AccountKey': 'c5SCu1KNQ4OsnpfU+wxoyg=='
+            }
+          });
+
+        console.log(response)
+
+        const data = await response.json()
+        console.log(data)
+
+    } catch(ex) {
+        console.log(ex)
+    }
+}
+// fetchDataAsync("https://api.opendota.com/api/teams")
+// fetchDataAsync("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139")
+fetchDataAsync("https://cors-anywhere.herokuapp.com/http://datamall2.mytransport.sg/ltaodataservice/BusStops")
